8114bca45a027ecfa3111c9d4524dade35e97961
KNoA <kura8165@gmail.com>
2022-03-24 09:24:31 +0900

    2022-03-24

--- functions/index.js
+++ functions/index.js
@@ -12,75 +12,82 @@ admin.initializeApp();
 // });
 
 exports.removeExpiredReservation =
-  functions.pubsub.schedule("every 1 minutes").onRun((context) => {
-    console.log("removeExpiredReservation run");
-    const db = admin.firestore();
-    const now = firestore.Timestamp.now();
-    // const ts = firestore.Timestamp.fromMillis(now.toMillis() - 3600000);
-    const ts = firestore.Timestamp.fromMillis(now.toMillis() - 60000);
+  functions.pubsub.schedule("every 1 minutes")
+      .timeZone("Asia/Seoul")
+      .onRun((context) => {
+        console.log("removeExpiredReservation run");
+        const db = admin.firestore();
+        const now = firestore.Timestamp.now();
+        // const ts = firestore.Timestamp.fromMillis(now.toMillis() - 3600000);
+        const ts = firestore.Timestamp.fromMillis(now.toMillis() - 180000);
 
-    const promises = [];
-    const readingRoom = db.collection("ReadingRoom");
-    const readingRoomSnap = readingRoom.get();
+        const promises = [];
+        const readingRoom = db.collection("ReadingRoom");
+        const readingRoomSnap = readingRoom.get();
 
-    // 만료된 Reserved 삭제(reservPublishedAt기준), 만료된 Seating 삭제()
-    readingRoomSnap.then((roomDoc) => {
-      roomDoc.forEach((room) => {
-        room.ref.collection("Seat")
-            .where("status", "!=", "Empty")
-            .get()
-            .then((seatDoc) => {
-              console.log("TS: " + ts.toMillis());
-              console.log("SeatDocSize: " + seatDoc.size);
-              seatDoc.forEach((seat) => {
-                if (seat.get("status") == "Reserved") {
-                  if (seat.get("reservPublishedAt") < ts.toMillis()) {
-                    seat.ref.update({status: "Empty", reservPublishedAt: 0});
-                    const snap = seat.ref.collection("Reservation").get();
-                    snap.then((snap) => {
-                      snap.forEach((dest) => {
-                        const user = db.collection("UserData")
-                            .doc(dest.get("reservated_user_id"))
-                            .collection("Reservation");
-                        const userSnap = user.get();
-
-                        userSnap.then((s1) => {
-                          s1.forEach((s2) => {
-                            s2.ref.delete();
-                          });
-                        });
-
-                        promises.push(dest.ref.delete());
-                      });
-                    });
-                  }
-                } else if (seat.get("status") == "Seating") {
-                  const snap = seat.ref.collection("Reservation").get();
-                  snap.then((snap) => {
-                    snap.forEach((dest) => {
-                      if (dest.get("expiredAt") < now.toMillis()) {
+        // 만료된 Reserved 삭제(reservPublishedAt기준), 만료된 Seating 삭제()
+        readingRoomSnap.then((roomDoc) => {
+          roomDoc.forEach((room) => {
+            room.ref.collection("Seat")
+                .where("status", "!=", "Empty")
+                .get()
+                .then((seatDoc) => {
+                  console.log("SeatDocSize: " + seatDoc.size);
+                  seatDoc.forEach((seat) => {
+                    if (seat.get("status") == "Reserved") {
+                      if (seat.get("reservPublishedAt") < ts.toMillis()) {
                         seat.ref
                             .update({status: "Empty", reservPublishedAt: 0});
-                        const user = db.collection("UserData")
-                            .doc(dest.get("reservated_user_id"))
-                            .collection("Reservation");
-                        const userSnap = user.get();
+                        const snap = seat.ref.collection("Reservation").get();
+                        snap.then((snap) => {
+                          snap.forEach((dest) => {
+                            const user = db.collection("UserData")
+                                .doc(dest.get("reservated_user_id"))
+                                .collection("Reservation");
+                            const userSnap =
+                                user.where("expiredAt", "<", now.toMillis())
+                                    .get();
 
-                        userSnap.then((s1) => {
-                          s1.forEach((s2) => {
-                            s2.ref.delete();
+                            userSnap.then((s1) => {
+                              s1.forEach((s2) => {
+                                s2.ref.delete();
+                              });
+                            });
+
+                            promises.push(dest.ref.delete());
                           });
                         });
-
-                        promises.push(dest.ref.delete());
                       }
-                    });
-                  });
-                }
-              });
-            });
-      });
-    });
+                    } else if (seat.get("status") == "Seating") {
+                      const snap = seat.ref.collection("Reservation").get();
+                      snap.then((snap) => {
+                        snap.forEach((dest) => {
+                          if (dest.get("expiredAt") < now.toMillis()) {
+                            seat.ref
+                                .update({status: "Empty",
+                                  reservPublishedAt: 0});
+                            const user = db.collection("UserData")
+                                .doc(dest.get("reservated_user_id"))
+                                .collection("Reservation");
+                            const userSnap =
+                                user.where("expiredAt", "<",
+                                    now.toMillis()).get();
 
-    return Promise.all(promises);
-  });
+                            userSnap.then((s1) => {
+                              s1.forEach((s2) => {
+                                s2.ref.delete();
+                              });
+                            });
+
+                            promises.push(dest.ref.delete());
+                          }
+                        });
+                      });
+                    }
+                  });
+                });
+          });
+        });
+
+        return Promise.all(promises);
+      });
